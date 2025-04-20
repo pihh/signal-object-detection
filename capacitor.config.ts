3e363fedfa4c8fd6cc8d61c54c743261974a8435
unknown <filipemotasa@hotmail.com>
2025-04-20 22:12:10 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vision.unconvetional',
+  appName: 'unconventional-vision',
+  webDir: 'www'
+};
+
+export default config;
